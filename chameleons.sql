25e14eabfc3cba5230bef9d736c5e1b929aa35d2
YaelRomanZ <164113890+YaelRomanZ@users.noreply.github.com>
2025-03-27 17:33:44 -0600

    Update chameleons.sql

--- chameleons.sql
+++ chameleons.sql
@@ -1,14 +1,96 @@
-CREATE DATABASE chameleons;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 27-03-2025 a las 04:29:01
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-USE chameleons;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE IF NOT EXISTS datos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido_paterno VARCHAR(100) NOT NULL,
-    apellido_materno VARCHAR(100),
-    telefono_celular VARCHAR(15),
-    email VARCHAR(100) NOT NULL UNIQUE,
-    contrasena VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `chameleons`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `datos`
+--
+
+CREATE TABLE `datos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido_paterno` varchar(50) NOT NULL,
+  `apellido_materno` varchar(50) NOT NULL,
+  `telefono_celular` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contrasena` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `datos`
+--
+
+INSERT INTO `datos` (`id`, `nombre`, `apellido_paterno`, `apellido_materno`, `telefono_celular`, `email`, `contrasena`) VALUES
+(2, 'Roman', 'Nieto', 'Martinez', '5592182809', 'ejemplo@gmail.com', '$2y$10$dNrYdNQRJ5SAs4HTcyuAmuCFu9hvGMFdRqQz0722/LFBhUyxbVv6m');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productoch`
+--
+
+CREATE TABLE `productoch` (
+  `idproducto` int(11) NOT NULL,
+  `nombreprod` varchar(100) NOT NULL,
+  `marcaprod` varchar(100) NOT NULL,
+  `tipoprod` varchar(100) NOT NULL,
+  `precioprod` decimal(10,2) NOT NULL,
+  `tallaprod` varchar(50) NOT NULL,
+  `categoria` varchar(50) DEFAULT NULL,
+  `stock` int(11) DEFAULT 0,
+  `descripcion` text DEFAULT NULL,
+  `imagenprod` blob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `datos`
+--
+ALTER TABLE `datos`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Indices de la tabla `productoch`
+--
+ALTER TABLE `productoch`
+  ADD PRIMARY KEY (`idproducto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `datos`
+--
+ALTER TABLE `datos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
